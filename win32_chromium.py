d3b245a7d07e5328ec4fbc8224cb3f4215066267
Cediddi <umutkarci@std.sehir.edu.tr>
2012-11-07 15:11:36 +0200

    Test commit

--- win32_chromium.py
+++ win32_chromium.py
@@ -1,64 +1,56 @@
-#! /usr/local/bin/python3
+#! /bin/python3
 # ------------------------------------------------------
 # |   Mighty Chromium Installer and Updater rel 1.3    |
 # ------------------------------------------------------
 # | This Mighty Python Script downloads and  installs  |
-# | or updates lastest Chromium build snapshot for OSX |
+# | or updates lastest Chromium build snapshot for Win |
 # ------------------------------------------------------
 # |           Copyright (c)  2012 Umut Karcı           |
 # |            <umutkarci@std.sehir.edu.tr>            |
 # ------------------------------------------------------
-
+   
 import shutil, urllib.request, zipfile, re, os, math, threading, time, sys
 from threading import Thread
 
-LAST=str(urllib.request.urlopen("https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/LAST_CHANGE").read())[2:-1]
-ZIP=("https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/"+LAST+"/chrome-mac.zip")
-ZIP_LOCAL=("/tmp/chrome-mac.zip")
-BIN_LOCAL=("/tmp/chrome-mac/Chromium.app")
-APP_DIR=("/Applications/")
-BIN_APP=(APP_DIR+"Chromium.app")
-PLIST_LOCAL=(BIN_APP+"/Contents/Info.plist")
+LAST=str(urllib.request.urlopen("https://commondatastorage.googleapis.com/chromium-browser-snapshots/Win/LAST_CHANGE").read())[2:-1]
+ZIP=("https://commondatastorage.googleapis.com/chromium-browser-snapshots/Win/"+LAST+"/chrome-win32.zip")
+ZIP_LOCAL=("C:\WINDOWS\Temp\chrome-win32.zip")
+BIN_LOCAL=("C:\WINDOWS\Temp\chrome-win32")
+APP_DIR=("C:\Program Files")
+BIN_APP=(APP_DIR+"\chrome-win32")
+VERSION_LOCAL=(BIN_APP+"\VERSION")
 
 def f_downloader():
 	print("Downloading")
 	Thread(target = t_sizeprinter).start()
+	if os.path.isfile(ZIP_LOCAL):
+		os.remove(ZIP_LOCAL)
+	open(ZIP_LOCAL,"w").close()
 	urllib.request.urlretrieve (ZIP, ZIP_LOCAL)
 	sys.stdout.write("\n")
 
 def f_extracter():
 	print("Extracting")
-	zipfile.ZipFile(ZIP_LOCAL).extractall("/tmp/")
+	zipfile.ZipFile(ZIP_LOCAL).extractall("C:\WINDOWS\Temp")
 
 def f_installer():
-	os.system("killall -9 Chromium");
+	f = open("C:\WINDOWS\Temp\chrome-win32\VERSION", "w")
+	f.write(LAST)
+	f.close()
+	os.system("TASKKILL /F /IM chrome.exe")
 	time.sleep(3)
-	if os.path.isfile(PLIST_LOCAL):
-		print("Installing")
+	if os.path.isdir(BIN_APP):
 		shutil.rmtree(BIN_APP)
-		shutil.move(BIN_LOCAL, APP_DIR)
-	else:
-		print("Installing")
-		shutil.move(BIN_LOCAL, APP_DIR)
-
-def f_permissioner():
-	print("Setting permissions")
-	for root, dirs, files in os.walk(BIN_APP):
-		for filevar in dirs:
-			os.chmod(os.path.join(root, filevar), 493)
-		for filevar in files:
-			os.chmod(os.path.join(root, filevar), 493)
+	print("Installing")
+	shutil.move(BIN_LOCAL, APP_DIR)
 
 def f_cleaner():
 	print("Removing temp files")
-	shutil.rmtree("/tmp/chrome-mac/")
 	os.remove(ZIP_LOCAL)
 
 def t_sizeprinter():
 	SIZE = math.floor(int(urllib.request.urlopen(ZIP).info().get('Content-Length')))
 	print("Total Size: ", math.floor(SIZE/1024**2), "MB")
-	if os.path.isfile(ZIP_LOCAL):
-		os.remove(ZIP_LOCAL)
 	SIZE_LOCAL_PERCENT = "0.00"
 	while SIZE_LOCAL_PERCENT != "100.":
 		if os.path.isfile(ZIP_LOCAL):
@@ -72,9 +64,9 @@ def t_sizeprinter():
 		else:
 			sys.stdout.write("Percent: %s %%  %s MB  \r" % ("0.00", "00"))
 
-if os.path.isfile(PLIST_LOCAL):
-	INFO_PLIST=open(PLIST_LOCAL, "r").read()
-	VER=str(re.findall("(?<=<string>)(\d{6})(?=</string>)", INFO_PLIST))[2:-2]
+if os.path.isfile(VERSION_LOCAL):
+	INFO_VERSION=open(VERSION_LOCAL, "r").read()
+	VER=str(INFO_VERSION)
 else:
 	VER="0"
 	print("App isn't installed yet.")
@@ -84,7 +76,6 @@ if int(LAST) > int(VER):
 	f_downloader()
 	f_extracter()
 	f_installer()
-	f_permissioner()
 	f_cleaner()
 else:
 	print("Already Up To Date")
